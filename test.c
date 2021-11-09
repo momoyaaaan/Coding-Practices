a1e7c7919124f67727016a74185b709295c949e9
momoyaaaan <90666686+momoyaaaan@users.noreply.github.com>
2021-11-09 17:03:30 +0800

    Create test.c

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    
+    float a = 0.0, b = 0.0;
+    float result;
+
+    printf("Enter the base of the pizza slice: ");
+    scanf("%f", &a);
+    
+    printf("Enter the height of the pizza slice: ");
+    scanf("%f", &b);
+    
+    
+    printf("Area of the pizza slice = %.2f", result = a*b/2);
+    
+
+    return 0;
+}
