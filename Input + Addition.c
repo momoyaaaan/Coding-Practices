d2ec61cff859bd6aa7cf6036986df343b9771933
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-09 21:44:06 +0800

    Create Input + Addition.c

---
+++ Input + Addition.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(void){
+    int a, b, c;
+    
+
+    printf("Enter the first integer: ");
+    scanf("%d", &a);
+
+    printf("Enter the second integer: ");
+    scanf("%d", &b);
+
+   
+    printf("Sum = %d", c = a+b);
+   
+
+    return 0;
+}
