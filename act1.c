d5ec1d678b5d0e13a4e7295742e490bf3a07bfcf
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-09 18:07:07 +0800

    Update act1.c

--- act1.c
+++ act1.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-    
-    float a = 0.0, b = 0.0;
-    float result;
+
+    float a, b;
+
 
     printf("Enter the base of the pizza slice: ");
     scanf("%f", &a);
-    
+
     printf("Enter the height of the pizza slice: ");
     scanf("%f", &b);
-    
-    
-    printf("Area of the pizza slice = %.2f", result = a*b/2);
-    
+
+
+    printf("Area of the pizza slice = %.2f", a*b/2);
+
 
     return 0;
 }
