2261c12a686bbc684d0fbfa81c37f38e002043c9
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-09 21:58:18 +0800

    Create Input Combination.c

---
+++ Input Combination.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    
+    char a, b;
+
+    printf("Enter the first character: ");
+    scanf("\n%c", &a);
+
+    printf("Enter the second character: ");
+    scanf("\n%c", &b);
+
+    
+
+    printf("%c%c", a, b);
+
+    return 0;
+}
