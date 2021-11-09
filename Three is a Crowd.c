554603a93d222bc9f196ef3bc707308beb66408f
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-09 21:43:11 +0800

    Create Three is a Crowd.c

---
+++ Three is a Crowd.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void) {
+    char aa, bb, cc;
+
+    printf("Enter the first character: ");
+    scanf("\n%c", &aa);
+
+    printf("Enter the second character: ");
+    scanf("\n%c", &bb);
+
+    printf("Enter the third character: ");
+    scanf("\n%c", &cc);
+
+
+
+
+    printf("%c \n%c \n%c", aa, bb, cc);
+
+
+    return 0;
+}
