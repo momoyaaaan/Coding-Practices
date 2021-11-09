d2142c0d80b2254383a0c2befa25db5769d5bb65
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-09 21:54:58 +0800

    Create Decimal x Decimal.c

---
+++ Decimal x Decimal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    
+    float a, b, c, d;
+
+    printf("Enter the first number: ");
+    scanf("%f", &a);
+
+    printf("Enter the second number: ");
+    scanf("%f", &b);
+
+    printf("Enter the third number: ");
+    scanf("%f", &c);
+
+    printf("Result = %.2f", d = a*b/c);
+
+    return 0;
+}
