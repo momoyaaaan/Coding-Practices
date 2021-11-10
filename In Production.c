3b00b1175792e73eea2bf3dddad5fc31b72e22b6
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-10 10:00:56 +0800

    Create In Production.c

---
+++ In Production.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+
+    float a, b, product;
+
+    printf("Enter the first number: ");
+    scanf("%f", &a);
+
+    printf("Enter the second number: ");
+    scanf("%f", &b);
+
+    printf("Product = %.2f", product = a*b);
+
+    return 0;
+}
