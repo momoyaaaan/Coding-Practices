c8c29a9d854044168cb5c44d87a7686bf9c18cd5
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-12 18:34:22 +0800

    Update Job Opportunity: Positions.c

--- Activity 1/Job Opportunity: Positions.c
+++ Activity 1/Job Opportunity: Positions.c
@@ -2,6 +2,29 @@
 
 int main(void){
 
+    char num,n1,n2,n3,n4,n5;
+
+    printf("Enter the 5-man team's numbers: ");
+    scanf("%c%c%c%c%c", &n1, &n2, &n3, &n4, &n5);
+
+    printf("%c %c %c %c %c", n1,n2,n3,n4,n5);
+
+    return 0;
+
+}
+
+
+
+
+// or with using pointers and while loop
+
+
+
+
+#include <stdio.h>
+
+int main(void){
+
     char number[126];
 
     printf("Enter the 5-man team's numbers: ");
