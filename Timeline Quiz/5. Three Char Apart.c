af0ea0736bb3c4e02325d6264c0067f42b385f75
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-12 17:27:44 +0800

    Create 5. Three Char Apart.c

---
+++ Timeline Quiz/5. Three Char Apart.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+
+    char a, b;
+
+    printf("Enter the first character: ");
+    scanf("\n%c", &a);
+
+    printf("Enter the second character: ");
+    scanf("\n%c", &b);
+
+    printf("%c\n.\n.\n.\n%c", a, b);
+
+    return 0;
+}
