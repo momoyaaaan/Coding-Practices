194862c0919895310e84f3783b0e1b0cbd712eb1
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-12 17:27:57 +0800

    Create 6. Flag Ceremony.c

---
+++ Timeline Quiz/6. Flag Ceremony.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void){
+
+    char a, b, c ,d ,e;
+
+    printf("Enter the first character: ");
+    scanf("\n%c", &a);
+
+    printf("Enter the second character: ");
+    scanf("\n%c", &b);
+
+    printf("Enter the third character: ");
+    scanf("\n%c", &c);
+
+    printf("Enter the fourth character: ");
+    scanf("\n%c", &d);
+
+    printf("Enter the fifth character: ");
+    scanf("\n%c", &e);
+
+    printf("%c %c %c %c %c", a, b, c, d ,e);
+
+    return 0;
+    
+}
