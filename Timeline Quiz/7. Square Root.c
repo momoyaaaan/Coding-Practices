eb532a9dfb67f740ad8884a799acd9c3cd468cde
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-12 17:28:08 +0800

    Create 7. Square Root.c

---
+++ Timeline Quiz/7. Square Root.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main(void){
+
+    float a, b;
+
+    printf("Enter n: ");
+    scanf("%f", &a);
+
+    printf("Square root of %.f = %.2f", a , b = sqrt(a) );
+
+    return 0;
+}
