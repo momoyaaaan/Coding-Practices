8d53ab7da565a1c122c7b11f2a46b10b1a9333cf
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-17 11:38:46 +0800

    Create Are Characters Created Equally?.c

---
+++ Conditions/Are Characters Created Equally?.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    char a, b;
+
+    printf("Enter the first character: ");
+    scanf("\n%c", &a);
+
+    printf("Enter the second character: ");
+    scanf("\n%c", &b);
+
+    printf("%c\\%c", a, b);
+
+    if(a == b){
+        printf("\nEqual");
+    }
+    
+    return 0;
+}
