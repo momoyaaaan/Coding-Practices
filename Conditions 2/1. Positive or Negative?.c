23c7b7426610bf10af8ce69335b0520810096f58
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-18 18:46:14 +0800

    Create 1. Positive or Negative?.c

---
+++ Conditions 2/1. Positive or Negative?.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+
+  int n;
+  
+  printf("Enter n: ");
+  scanf("%d", &n);
+  
+  if(n > 0){
+    printf("Positive");
+    }
+  else{
+    printf("Negative");
+    }
+   return 0;
+}
