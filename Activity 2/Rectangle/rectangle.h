4db8cf63fa4dd1eaf4311a53c3787f94cc9547e7
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-19 16:01:23 +0800

    Create rectangle.h

---
+++ Activity 2/Rectangle/rectangle.h
@@ -0,0 +1,5 @@
+float areaRect(float length, float width);
+float periRect(float length, float width);
+float diagLine(float length, float width);
+float areaTria(float length,float width);
+void display(float areaR, float peri, float diag, float areaT);
