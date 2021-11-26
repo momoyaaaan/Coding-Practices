a305d77ea6c2f4bd550bf33757d3a4d5577f1680
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-26 12:00:24 +0800

    Create digitPos.h

---
+++ Midterm Examination/3. 2 Raise to x (with Boilerplates)/digitPos.h
@@ -0,0 +1,14 @@
+int getNum();
+/*asks and returns number from the user.*/
+
+int getPos();
+/*asks and returns digit position*/
+
+int determineDigit(int num, int pos);
+/*returns the digit in the pos position*/
+
+int raiseTo(int digit);
+/*returns 2digit*/
+
+void display(int n);
+/*display the output*/
