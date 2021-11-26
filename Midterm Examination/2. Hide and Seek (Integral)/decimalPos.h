8ddb0c41f3be611d448bd0b1552c27304c7ba94d
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-26 11:58:53 +0800

    Create decimalPos.h

---
+++ Midterm Examination/2. Hide and Seek (Integral)/decimalPos.h
@@ -0,0 +1,11 @@
+float getNum();
+/*asks and returns number from the user.*/
+
+int getPos();
+/*asks and returns the integral position*/
+
+int determineNum(float num, int pos);
+/*returns the decimal value at the inputted integral position*/
+
+void display(int n);
+/*display the output*/
