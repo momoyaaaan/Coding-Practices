c665b90892d37c8a2088cafc6252b501d7123bb3
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-26 11:54:11 +0800

    Create square.h

---
+++ Practical Quiz 2/2. Hey Rhea/square.h
@@ -0,0 +1,7 @@
+float getSide();
+/*get input from the user.*/
+
+float solveArea(float side);
+/* compute and returns the area of the square.*/
+void display(float area);
+/*display the computed area*/
