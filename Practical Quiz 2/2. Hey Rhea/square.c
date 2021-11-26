a7b22f2ce15454dd3e940177e58969cdcdfe224a
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-11-26 11:55:50 +0800

    Create square.c

---
+++ Practical Quiz 2/2. Hey Rhea/square.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+float getSide(){
+    float gs;
+    
+    scanf("%f", &gs);
+}
+
+float solveArea(float side){
+    float sa, gs;
+    sa = gs*gs;
+    return sa;
+
+}
+void display(float area){
+    printf("%.2f", area);
+
+    return;
+}
