3a0eefc4c56bd66fb1c7442a57ff411c9d4355f7
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-12-02 11:26:51 +0800

    Create numberGame.h

---
+++ Activity 3/3. Yes Yes No(from codeChum, modified)/numberGame.h
@@ -0,0 +1,13 @@
+int getNum();
+/*
+accepts input from the user
+*/
+
+int identify(int num);
+/*
+    returns 1 if num is positive, odd, and is in between 15 to 30 (inclusive), else, returns 0.
+*/
+void display(int res);
+/*
+    displays yes if res equals 1, else, displays 0.
+*/
