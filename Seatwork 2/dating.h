35e435969089f8833db802354c8a3ddf71872984
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-12-02 11:27:43 +0800

    Create dating.h

---
+++ Seatwork 2/dating.h
@@ -0,0 +1,9 @@
+int isDivisible(int num1, int num2);
+/*
+    returns 1 if the second number is divisible with the first number,
+    else, returns 0.
+*/
+void display(int res);
+/*
+    displays "divisible" if res equals 1, else, displays "not". 
+*/
