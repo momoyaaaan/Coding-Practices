9d7afe1758693373d07305b5c019391fe140cbff
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-12-02 11:26:14 +0800

    Create leapYear.h

---
+++ Activity 3/2. The Leap of Year(from codeChum, modified)/leapYear.h
@@ -0,0 +1,8 @@
+int getYear();
+/*accepts year input from the user*/
+
+int isLeapYear(int year);
+/*returns 1 if year is a leap year, else, returns 0*/
+
+void display(int l);
+/*prints "Leap Year" if l equals 1, else prints "Not a Leap Year"*/
