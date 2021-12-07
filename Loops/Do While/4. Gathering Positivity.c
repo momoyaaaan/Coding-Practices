835662a105f823a335dd73fb92c6c306823765d4
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-12-07 18:40:26 +0800

    Create 4. Gathering Positivity.c

---
+++ Loops/Do While/4. Gathering Positivity.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    int num, sum = 0;
+
+    do{
+        printf("Enter n: ");
+        scanf("%d", &num);
+
+        
+        if(num > 0){
+            sum+=num;
+        }
+
+    }while(num != 0);
+    printf("Total of all positives = %d", sum);
+    return 0;
+}
