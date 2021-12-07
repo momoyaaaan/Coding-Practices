fd596ab8c02fb97b7d0f96a9bce86871e2fb6840
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-12-07 18:38:14 +0800

    Create 1. Broken Record.c

---
+++ Loops/While Loop/1. Broken Record.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int a;
+    int i = 1;
+
+    printf("Enter n: ");
+    scanf("%d", &a);
+
+    while(i <= a){
+        printf("CodeChum is awesome\n");
+        i++;
+    }
+    return 0;
+
+}
