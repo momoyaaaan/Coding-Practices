d39d472893542d417b2fa832f2f224129575e08a
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-12-07 10:42:13 +0800

    Delete Practical Quiz/1. Mysterious Blue (from codeChum, modified) directory

--- Practical Quiz/1. Mysterious Blue (from codeChum, modified)/age.h
+++
@@ -1,4 +0,0 @@
-int getAge();
-/*get input from user*/
-void display(int age);
-/*display minor, adult or senior depending on the age*/
--- Practical Quiz/1. Mysterious Blue (from codeChum, modified)/main.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include"age.h"
-int main(void) {
-    int age = getAge();
-    display(age);
-    return 0;
-}
