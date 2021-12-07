f945662c413d7815b1d8832acd14206ce5d4a2df
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-12-07 10:45:12 +0800

    Create age.h

---
+++ Practical Quiz 3/3. Age Category(from codeChum, modified)/age.h
@@ -0,0 +1,10 @@
+int getAge();
+/*get age as input from the user*/
+
+void display(int age);
+/*prints the following:
+Child - 0 to 12 years old
+Teenager - 13 to 17 years old
+Adult - 18 to 59 years old
+Senior Citizen - 60 years old and above
+*/
