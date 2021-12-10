8419ffe3ff49b9101f4b8a1c06f0eb31ea97cd63
Ian <90666686+momoyaaaan@users.noreply.github.com>
2021-12-10 17:13:02 +0800

    Create decBin.h

---
+++ Activity 4/2. DecBin(from codeChum, modified)/decBin.h
@@ -0,0 +1,11 @@
+int getNum();
+/*accepts input from the user.*/
+
+long decBin(int n);
+/*
+    returns the binary equivalent of n. 
+    Note: Use of built in functions that will automatically convert decimal to binary is not allowed.
+*/
+
+void display(long binary);
+/*displays the binary equivalent*/
